ae06e87504b400747783a97d572d24ac9470c494
AndreQ <drol.005@gmail.com>
2014-02-06 11:45:25 -0500

    the less operator now is setted for all the table.\n + comments \n + UI changed \n

--- refactor.js
+++ refactor.js
@@ -1,3 +1,4 @@
+// linked list, usefull to do a clean swap in the table but could be replaced for an array
 var linked_list = function () {
 
 	var node_methods = {
@@ -73,7 +74,7 @@ var linked_list = function () {
 };
 
 var board_handler = (function () {
-
+  // i miss my c++ iteration macross ):
 	var rep = function (i, n, f) {
 		for (var x = i; x < n; x++) {
 			f(x);
@@ -84,13 +85,17 @@ var board_handler = (function () {
 		body : null, 
 		lock : null, 
 		node_list : null, 
+    less_than : null, 
 		in_transition : false,
-		init_table : function () {
+    // starts the mess
+		init_table : function (less_op) {
+      this.less_than = less_op || function () { return false; };
 			this.body = scoreboard_table.childNodes[3];
 			this.lock = [];
 			this.node_list = linked_list();
 			in_transition = false;
 		}, 
+    // cleans the mess
 		clear : function () {
 			this.lock.length = 0;
 			this.node_list.init();
@@ -98,6 +103,7 @@ var board_handler = (function () {
 				this.body.removeChild(this.body.lastChild);
 			in_transition = false;
 		},
+    // transition to swap the rows, only supports webkit. moz and others should be added
 		transition : function (e, movement, callback) {
 			movement = -movement;
 			e.style["webkitTransition"] = "-webkit-transform";
@@ -117,7 +123,9 @@ var board_handler = (function () {
 			})();
 			e.addEventListener('webkitTransitionEnd', auto_delete);
 		},
-		move_up : function (e, less_than, next_it) {
+    // moves up a row, the less_than function defines if it keeps going up or stops
+    // next_it is a function called when the row can't go any higher
+		move_up : function (e, next_it) {
 			next_it = next_it || (function () {});
 			if (!e.has_prev()) {
 				next_it();
@@ -127,7 +135,7 @@ var board_handler = (function () {
 
 			var dom_e = e.dom_e;
 			var prev = e.prev;
-			if (!less_than(e, prev)) {
+			if (!table.less_than(e, prev)) {
 				next_it();
 				return;
 			}
@@ -139,28 +147,33 @@ var board_handler = (function () {
 			rep (0, ids.length, function (id) { table.lock[ids[id]]++; });
 
 			this.transition(prev_dom, -(prev_dom.offsetHeight + 2), function(){});
-
+    
 			this.transition(dom_e, dom_e.offsetHeight + 2,
 					function () {
 						table.node_list.unlink(e);
 						table.node_list.link(e, prev);
 						table.body.insertBefore(e.dom_e, prev.dom_e);
 						table.in_transition = false;
-
+            var order = e.order;
+            e.order = prev.order;
+            prev.order = order;
+            e.dom_e.childNodes[0].textContent = e.order+1;
+            prev.dom_e.childNodes[0].textContent = prev.order+1;
 						rep (0, ids.length, 
 							function (id) { table.lock[ids[id]]--; }
 						);
-						if (!e.has_prev() || less_than(e, e.prev))
-							table.move_up(e, less_than, next_it);
+						if (!e.has_prev() || table.less_than(e, e.prev))
+							table.move_up(e, next_it);
 						else next_it();
 					}
 			);
 		},
+    // adds a dummy row, also needs to be replaced for a call to a parser
 		add_row : function (e) {
 			var _id = this.node_list.length;
 			var node = this.node_list.new_node();
 			var row = Object.create(node);
-
+      row.order = _id;
 			row.id = _id;
 			row.data = e;
 			row.dom_e = (function () {
@@ -174,23 +187,26 @@ var board_handler = (function () {
 
 					dom_row.classList.add('inactive');
 					dom_row.onclick = function () {
-						table.move_up(row, function (a, b) {
-							return a.data[0] < b.data[0];
-						});
+						table.move_up(row);
 					};
 
 					return dom_row;
 			})();
-
+      // reveals the judgement of a submition, it would be replaced for the actual query to a parser or something
       row.reveal = function () {
         var data = row.data;
         for (var idx = 1; idx < data.length; idx++) {
           if (data[idx] === '-') {
             data[idx] = Math.floor(Math.random()*2);
             row.dom_e.childNodes[idx].textContent = data[idx];
-            data[0] += data[idx];
-            row.dom_e.childNodes[0].textContent = data[0];
-            if (data[idx] > 0) return true;
+            data[2] += data[idx];
+            row.dom_e.childNodes[2].textContent = data[2];
+            if (data[idx] > 0) {
+              data[3] += Math.floor(Math.random()*10) + 10;
+              row.dom_e.childNodes[3] = data[3];
+              row.dom_e.childNodes[3].textContent = data[3];
+              return true;
+            }
           } 
         }
         return false;
@@ -203,9 +219,17 @@ var board_handler = (function () {
 		}
 	};
 
+  // mock less operator used everywhere
+  var less = function (a, b) {
+      if (a.data[2] !== b.data[2])
+       return a.data[2] > b.data[2];
+      return a.data[3] < b.data[3];
+  };
+
+  // setup and auto sorting animations
 	var _o = {
 		init : function () {
-			table.init_table();
+			table.init_table(less);
 			genButton.onclick = function () {
 				var rows = parseInt(rowNum.value, 10);
 				if (isNaN(rows)) return;
@@ -213,7 +237,7 @@ var board_handler = (function () {
 				table.clear();
 				rep (0, rows, function (id) {
 					var xd = rows - id;
-					table.add_row([xd, "Name "+id, "-", "-", "-"]);
+					table.add_row([id + 1, "Name "+id, xd, xd * 10, "-", "-", "-"]);
 				});
 				_o.play(table.node_list.rbegin());
 			};
@@ -222,21 +246,15 @@ var board_handler = (function () {
 			table.move_up(id);
 		},
 		play : function (node) {
-      var less = function (a, b) {
-              return a.data[0] > b.data[0];
-            };
-
-			var dom = node.dom_e;
+ 			var dom = node.dom_e;
 			dom.classList.remove('inactive');
 			dom.classList.add('active');
 			var next = node.prev;
 			var pode = node.has_prev();
-      while (node.reveal() && (pode && !less(node, next))) {
-        
-      }
+      while (node.reveal() && pode && !less(node, next)) {}
+      if (!pode) while (node.reveal());
       setTimeout(function () {
         table.move_up(node, 
-            less, 
             function () {
               dom.classList.remove('active');
               dom.classList.add('inactive');
